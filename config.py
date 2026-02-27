12b58a7adeb933982f5bf28b0447bf6af6f1b984
илюха <abajon250@gmail.com>
2026-02-27 16:22:10 +0200

    Add GITHUB_TOKEN to config.py

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Токен бота Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Список администраторов (ID Telegram)
 ADMIN_IDS = []
 admin_str = os.getenv("ADMIN_IDS", "")
 if admin_str:
@@ -15,24 +13,22 @@ if admin_str:
         if part.isdigit():
             ADMIN_IDS.append(int(part))
 
-# Данные для Яндекс.Директ (если используются)
 YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN")
 YANDEX_CLIENT_ID = os.getenv("YANDEX_CLIENT_ID")
 YANDEX_LOGIN = os.getenv("YANDEX_LOGIN")
 
-# Пути для лендингов
 LANDING_STORAGE_PATH = os.getenv("LANDING_STORAGE_PATH", "landings")
 LANDING_BASE_URL = os.getenv("LANDING_BASE_URL", "http://localhost/landings/")
 
-# Данные для ADB (MAX)
 ADB_PATH = os.getenv("ADB_PATH", "adb")
 MAX_PACKAGE = os.getenv("MAX_PACKAGE")
 MAX_ACTIVITY = os.getenv("MAX_ACTIVITY")
 
-# Telegram API для авторизации пользователей
 TG_API_ID = int(os.getenv("TG_API_ID", 0))
 TG_API_HASH = os.getenv("TG_API_HASH", "")
 
-# Токены платёжных систем
 CRYPTO_PAY_TOKEN = os.getenv("CRYPTO_PAY_TOKEN")
 XROCKET_API_KEY = os.getenv("XROCKET_API_KEY")
+
+# Добавляем эту строку
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
